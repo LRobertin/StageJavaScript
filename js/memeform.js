1e4370c2bfd557ecea838b906dc72ec3156f8963
LRobertin <ludovic.robertin@gmail.com>
2023-06-27 15:03:21 +0200

    maj renderMeme

--- js/memeform.js
+++ js/memeform.js
@@ -6,40 +6,58 @@ function initMemeEditor() {
 
   form["titre"].addEventListener("input", function (evt) {
     currentMeme.titre = evt.target.value;
+    renderMeme();
   });
   form["imageId"].addEventListener("input", function (evt) {
     currentMeme.imageId = evt.target.value;
+    renderMeme();
   });
   form["text"].addEventListener("input", function (evt) {
     currentMeme.text = evt.target.value;
+    renderMeme();
   });
   form["x"].addEventListener("input", function (evt) {
     currentMeme.x = Number(evt.target.value);
+    renderMeme();
   });
   form["y"].addEventListener("input", function (evt) {
     currentMeme.y = Number(evt.target.value);
+    renderMeme();
   });
   form["color"].addEventListener("input", function (evt) {
     currentMeme.color = evt.target.value;
-    renderMeme(currentMeme);
+    renderMeme();
   });
   form["font-size"].addEventListener("input", function (evt) {
     currentMeme.fontsize = Number(evt.target.value);
+    renderMeme();
   });
   form["font-weight"].addEventListener("input", function (evt) {
     currentMeme.fontweight = Number(evt.target.value);
+    renderMeme();
   });
   form["underline"].addEventListener("input", function (evt) {
     currentMeme.underline = evt.target.checked;
+    renderMeme();
   });
   form["italic"].addEventListener("input", function (evt) {
     currentMeme.italic = evt.target.checked;
+    renderMeme();
   });
 }
 
 function renderMeme(meme) {
-       var svg =  document.querySelector("#editor-viewer svg");
-       var textElement = svg.querySelector('text');
-       textElement.style.fill = meme.color;
-       textElement.style.underline = meme.underline ? "underline" : "none"; 
+  if (undefined === meme) {
+    meme = currentMeme;
+  }
+  var svg = document.querySelector("#editor-viewer svg");
+  var textElement = svg.querySelector("text");
+  textElement.style.fill = meme.color;
+  textElement.innerHTML = meme.text;
+  textElement.style.textDecoration = meme.underline ? "underline" : "none";
+  textElement.setAttribute('font-weight', meme.fontweight)
+  textElement.setAttribute('font-size', meme.fontsize)
+  //textElement.setAttribute('font-style', meme.italic)
+  textElement.setAttribute('x', meme.x)
+  textElement.setAttribute('y', meme.y)
 }
