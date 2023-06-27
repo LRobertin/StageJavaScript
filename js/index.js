26aaa126ddb4b7c4896bc724308881b478116d92
LRobertin <ludovic.robertin@gmail.com>
2023-06-27 11:07:09 +0200

    sync darkmode w/ slider + nav+ body

--- js/index.js
+++ js/index.js
@@ -1,18 +1,37 @@
-function init(){
-    var x = document.getElementsByTagName('footer')[0]
-    var currentDate = new Date().toISOString()
-    var style = x.style
-    x.innerHTML = currentDate
-    style.textDecoration='underline'
-    style.fontStyle='italic'
-    style.backgroundColor='rgb(128,128,128,0.1)'
-    console.log(x)
+function init() {
+  var x = document.getElementsByTagName("footer")[0];
+  var currentDate = new Date().toISOString();
+  var style = x.style;
+  x.innerHTML = currentDate;
+  style.textDecoration = "underline";
+  style.fontStyle = "italic";
+  style.backgroundColor = "rgba(128,128,128,0.1)";
+  console.log(x);
 }
 
-document.addEventListener('DOMContentLoaded', function(evt){
-    console.log(evt)
-    init()
-})
+document.addEventListener("DOMContentLoaded", function (evt) {
+  console.log(evt);
+  init();
+});
 
-/*
-*/
+/**
+ *
+ * @param {boolean} isDark
+ */
+
+function changeTheme(isDark) {
+  var nav = document.getElementsByTagName("nav")[0];
+  var btn = document.getElementById("theme-switch");
+
+  if (isDark) {
+    document.body.className = "dark";
+    nav.classList.replace("bg-light", "bg-dark");
+    nav.classList.replace("navbar-light", "navbar-dark");
+    btn.checked = true;
+  } else {
+    document.body.className = "";
+    nav.classList.replace("bg-dark", "bg-light");
+    nav.classList.replace("navbar-dark", "navbar-light");
+    btn.checked = false;
+  }
+}
