66410e4bcecc7ac23339ca532a19662183294ee6
LRobertin <ludovic.robertin@gmail.com>
2023-06-27 09:16:46 +0200

    modif content footer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-alert('hello')
+function init(){
+    var x = document.getElementsByTagName('footer')[0]
+    var currentDate = new Date().toISOString()
+    x.innerHTML = currentDate
+    console.log(x)
+}
+init()
