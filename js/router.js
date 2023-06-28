a92c5f9ee312958ee7c87bf92e68ca809391fdfb
LRobertin <ludovic.robertin@gmail.com>
2023-06-28 10:59:30 +0200

    instance recup chemin

---
+++ js/router.js
@@ -0,0 +1,15 @@
+/**
+ * Variable de config des routes
+ */
+
+const routeConfig = {
+  routes: [],
+};
+/**
+ *
+ * Fonction de maintient de la route
+ */
+export const handleRoute = (params) => {
+  const pathName = location.pathname;
+  console.log(pathName);
+};
