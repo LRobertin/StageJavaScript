b67d1bab55cc4fb85c24ba41fc707bd184a6f08a
LRobertin <ludovic.robertin@gmail.com>
2023-06-28 11:14:07 +0200

    passe objet + def des routes

--- js/router.js
+++ js/router.js
@@ -3,7 +3,23 @@
  */
 
 const routeConfig = {
-  routes: [],
+  routes: [
+    {
+      path: "/thumbnail",
+      initialisation: undefined,
+      templateUrl: "/view/thumbnail.html",
+    },
+    {
+      path: "/",
+      initialisation: undefined,
+      templateUrl: "/view/home.html",
+    },
+    {
+      path: "/break",
+      initialisation: undefined,
+      templateUrl: "/vue/templateQuiExistePasSurLeServeur.html",
+    },
+  ],
 };
 /**
  *
@@ -13,3 +29,21 @@ export const handleRoute = (params) => {
   const pathName = location.pathname;
   console.log(pathName);
 };
+
+class Router {
+  /**
+   * Manage la route en cours
+   */
+  handleRoute() {
+    const pathName = location.pathname;
+    console.log(pathName);
+  }
+  /**
+   * Navigate to
+   * @param {string} pathName chemin commencant par
+   */
+  changeRoute(pathName) {}
+}
+
+const router = new Router();
+router.handleRoute();
